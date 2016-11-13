774fd487002169a29e92c46a88658069688fbd29
tunghim <tunghim@gmail.com>
2016-11-14 00:44:44 +0800

    Exception Handling

---
+++ Java Exception Handling (Try-catch)_Sol.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        try {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x / y);
+        } catch(InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
